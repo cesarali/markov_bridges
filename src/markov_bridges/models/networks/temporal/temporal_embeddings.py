0f83c2b653f2e97946a71304c14689d438e09cd0
Darius Faroughy <dariusfar@gmail.com>
2024-08-05 18:05:48 -0400

    added sinusoidal and Fourier time embeddings

--- src/markov_bridges/models/networks/temporal/temporal_embeddings.py
+++ src/markov_bridges/models/networks/temporal/temporal_embeddings.py
@@ -1,5 +1,6 @@
 import math
 import torch
+import torch.nn as nn
 from torch.nn import functional as F
 
 def transformer_timestep_embedding(timesteps, embedding_dim, max_positions=10000):
@@ -14,3 +15,35 @@ def transformer_timestep_embedding(timesteps, embedding_dim, max_positions=10000
         emb = F.pad(emb, (0, 1), mode='constant')
     assert emb.shape == (timesteps.shape[0], embedding_dim)
     return emb
+
+
+def timestep_sinusoidal_embedding(timesteps, dim, max_period=10000):
+    """Create sinusoidal timestep embeddings.
+
+    :param timesteps: a 1-D Tensor of N indices, one per batch element. These may be fractional.
+    :param dim: the dimension of the output.
+    :param max_period: controls the minimum frequency of the embeddings.
+    :return: an [N x dim] Tensor of positional embeddings.
+    """
+    half = dim // 2
+    freqs = torch.exp( -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32, device=timesteps.device) / half)
+    args = timesteps[:, None].float() * freqs[None]
+    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+    if dim % 2:
+        embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+    return embedding
+
+
+class GaussianFourierProjection(nn.Module):
+    """Gaussian random features for encoding time steps.
+
+    Inspired by https://colab.research.google.com/drive/120kYYBOVa1i0TD85RjlEkFjaWDxSFUx3?usp=sharing#scrollTo=YyQtV7155Nht
+    """
+
+    def __init__(self, embed_dim, scale=30.0, device='cpu'):
+        super().__init__()
+        self.W = nn.Parameter(torch.randn(embed_dim // 2, device=device) * scale, requires_grad=False)
+
+    def forward(self, x):
+        x_proj = x[..., None] * self.W[None, ...] * 2 * math.pi
+        return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
